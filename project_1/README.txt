13974d2bc531331610b9700ac9b79fefcaf17f4c
gwynniehayes <hayes5@stolaf.edu>
2026-03-18 20:52:22 -0500

    project 1

---
+++ project_1/README.txt
@@ -0,0 +1,4 @@
+To test my algorithm I wrote a testing algorithm similar to one that we were given in a homework assignment. 
+To use this test it runs my algorithm on different size and length arrays that I inputed using as many different 
+scenarios that I could think of. To see if the algorithm worked for each of my test I outputed the original array 
+that was inputed then the sorted array in order to see if it was sorted..
--- project_1/main.cpp
+++ project_1/main.cpp
@@ -3,17 +3,50 @@
 
 using namespace std;
 
-int main() {
-    int arr[] = {3, 1, 2, 0, 3, 2, 1, 0};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
+void runTest(int arr[], int n, const string& testName) {
+    cout << testName << " - Original: ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    
     Sort(arr, n);
-
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
+    
+    cout << " | Sorted: ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
     cout << endl;
+}
+
+int main() {
+    int test1[] = {3, 1, 2, 0, 3, 2, 1, 0};
+    runTest(test1, 8, "Test 1: Mixed");
+
+    int test2[] = {0, 0, 1, 1, 2, 2, 3, 3};
+    runTest(test2, 8, "Test 2: Already sorted");
+
+    int test3[] = {3, 3, 2, 2, 1, 1, 0, 0};
+    runTest(test3, 8, "Test 3: Reverse sorted");
+
+    int test4[] = {0, 0, 0, 0};
+    runTest(test4, 4, "Test 4: All zeros");
+
+    int test5[] = {3, 3, 3, 3};
+    runTest(test5, 4, "Test 5: All threes");
+
+    int test6[] = {2};
+    runTest(test6, 1, "Test 6: Single element");
+
+    int test7[] = {1, 0};
+    runTest(test7, 2, "Test 7: Two elements");
+
+    int test8[] = {0, 1, 1, 0, 0, 1};
+    runTest(test8, 6, "Test 8: Zeros and ones");
+
+    int test9[] = {3, 2, 3, 2, 2, 3};
+    runTest(test9, 6, "Test 9: Twos and threes");
+
+    int test10[] = {};
+    runTest(test10, 0, "Test 10: Empty array");
+
+    int test11[] = {3, 0, 2, 1, 3, 1, 0, 2, 0, 1, 3, 2, 0};
+    runTest(test11, 13, "Test 11: Random large");
 
     return 0;
 }
